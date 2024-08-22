237f96a9a42bc11330d399ecf656536996937736
Steve-Daniels84 <steve@bleachcyber.com>
2024-08-22 14:51:02 +0100

    Updates

---
+++ precache-manifest.cc4fe23cf9f0260a6d91d4a88016829f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b83dbd157cb70a8f774c53858ea960",
+    "url": "/meetapp/index.html"
+  },
+  {
+    "revision": "d83c57129ff765e8b92e",
+    "url": "/meetapp/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "8dc44b16556b43a1d3c9",
+    "url": "/meetapp/static/js/2.11da216a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/meetapp/static/js/2.11da216a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbf04646e0819ab6a5c",
+    "url": "/meetapp/static/js/3.78155c6f.chunk.js"
+  },
+  {
+    "revision": "d83c57129ff765e8b92e",
+    "url": "/meetapp/static/js/main.04bcd6f4.chunk.js"
+  },
+  {
+    "revision": "ca1ca9a5d1e84ec06980",
+    "url": "/meetapp/static/js/runtime-main.3bd90e7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
