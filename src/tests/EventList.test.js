b3cb2adf4236fc6ea36008f5b041bdfad2823f57
Steve-Daniels84 <steve@bleachcyber.com>
2024-09-11 10:35:54 +0100

    adding esLint

--- src/tests/EventList.test.js
+++ src/tests/EventList.test.js
@@ -1,44 +1,44 @@
 import React from "react";
 import { render, waitFor, within } from "@testing-library/react";
-import { getEvents } from '../api';
+import { getEvents } from "../api";
 import EventList from "../components/EventList";
-import App from "../App"
+import App from "../App";
 
-describe('<EventList /> component', () => {
+describe("<EventList /> component", () => {
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
+  });
 
-    let EventListComponent;
-    beforeEach(() => {
-        EventListComponent = render(<EventList />)
-    })
+  test('has an element with "list" role', () => {
+    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  });
 
-    test('has an element with "list" role', () => {
-        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
-    });
-
-    test('renders correct number of events', async () => {
-        const allEvents = await getEvents(); 
-        EventListComponent.rerender(<EventList events={allEvents} />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
-      });
+  test("renders correct number of events", async () => {
+    const allEvents = await getEvents();
+    EventListComponent.rerender(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(
+      allEvents.length
+    );
+  });
 });
 
-describe('<EventList /> Integration', () => {
-    
-    global.MutationObserver = class {
-        constructor(callback) {
-          this.callback = callback;
-        }
-        observe() {}
-        disconnect() {}
-      };
+describe("<EventList /> Integration", () => {
+  global.MutationObserver = class {
+    constructor(callback) {
+      this.callback = callback;
+    }
+    observe() {}
+    disconnect() {}
+  };
 
-    test('renders a list of 32 events when the app is mounted and rendered', async () => {
-        const AppComponent = render (<App />);
-        const AppDom = AppComponent.container.firstChild;
-        const EventListDom = AppDom.querySelector('#event-list');
-        await waitFor(() => {
-            const EventListItems = within(EventListDom).queryAllByRole('listitem');
-            expect(EventListItems.length).toBe(32);
-        })
-    })
-})
+  test("renders a list of 32 events when the app is mounted and rendered", async () => {
+    const AppComponent = render(<App />);
+    const AppDom = AppComponent.container.firstChild;
+    const EventListDom = AppDom.querySelector("#event-list");
+    await waitFor(() => {
+      const EventListItems = within(EventListDom).queryAllByRole("listitem");
+      expect(EventListItems.length).toBe(32);
+    });
+  });
+});
