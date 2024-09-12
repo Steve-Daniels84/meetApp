602c1fedce719d2d9a4b1ad869cc97ec35e28d0f
Steve-Daniels84 <steve@bleachcyber.com>
2024-09-12 15:24:00 +0100

    Auth

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentCity]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ export const extractLocations = (events) => {
 
 export const checkToken = async (accessToken) => {
   const response = await fetch(
-    "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}"
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   );
   const result = await response.json();
   return result;
@@ -32,13 +32,11 @@ export const removeQuery = () => {
 
 //fetches all events data
 export const getEvents = async () => {
-//   if (window.location.href.startsWith("http://localhost")) {
-//     return mockData;
-//   }
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
 
   const token = await getToken();
-
-  console.log(token)
 
   if (token) {
     removeQuery();
@@ -54,23 +52,43 @@ export const getEvents = async () => {
   }
 };
 
+export const getAccessToken = async () => {             
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
 
 const getToken = async (code) => {
-    try {
-      const encodeCode = encodeURIComponent(code);
-   
-      const url = "https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/get-auth-url";
-      // eslint-disable-next-line no-useless-concat
-  
-      const getUrl = `${url}` + "/" + `${encodeCode}`;
-      const response = await fetch(getUrl);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      const { access_token } = await response.json();
-      access_token && localStorage.setItem("access_token", access_token);
-      return access_token;
-    } catch (error) {
-      return error;
+  try {
+    const encodeCode = encodeURIComponent(code);
+ 
+    const url = "https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/token";
+    // eslint-disable-next-line no-useless-concat
+
+    const getUrl = `${url}` + "/" + `${encodeCode}`;
+    const response = await fetch(getUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
     }
-  }; 
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    return error;
+  }
+}; 
