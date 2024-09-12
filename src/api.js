196b8118a2008ab886e3974be9b918289890c504
Steve-Daniels84 <steve@bleachcyber.com>
2024-09-12 12:07:54 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ export const extractLocations = (events) => {
 
 export const checkToken = async (accessToken) => {
   const response = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}"
   );
   const result = await response.json();
   return result;
@@ -38,6 +38,8 @@ export const getEvents = async () => {
 
   const token = await getToken();
 
+  console.log(token)
+
   if (token) {
     removeQuery();
     const url =
@@ -52,24 +54,23 @@ export const getEvents = async () => {
   }
 };
 
-export const getToken = async () => {
-  const accessToken = localStorage.getItem("access_token");
-  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/get-auth-url"
-      );
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
+const getToken = async (code) => {
+    try {
+      const encodeCode = encodeURIComponent(code);
+   
+      const url = "https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/get-auth-url";
+      // eslint-disable-next-line no-useless-concat
+  
+      const getUrl = `${url}` + "/" + `${encodeCode}`;
+      const response = await fetch(getUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const { access_token } = await response.json();
+      access_token && localStorage.setItem("access_token", access_token);
+      return access_token;
+    } catch (error) {
+      return error;
     }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
-
+  }; 
