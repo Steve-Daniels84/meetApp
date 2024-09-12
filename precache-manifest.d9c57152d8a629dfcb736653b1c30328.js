c051780d058d5ae1a81c027fd4d08b65a6b4e112
Steve-Daniels84 <steve@bleachcyber.com>
2024-09-12 17:01:21 +0100

    Updates

---
+++ precache-manifest.d9c57152d8a629dfcb736653b1c30328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7a243ce64ebfba9ffb5b018ce2a136",
+    "url": "/meetapp/index.html"
+  },
+  {
+    "revision": "20ca87567928d131a7a3",
+    "url": "/meetapp/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "162b94b1db12608077a2",
+    "url": "/meetapp/static/js/2.6c62386b.chunk.js"
+  },
+  {
+    "revision": "ca0926b29a65eafc016a6f4f5fe2a9cb",
+    "url": "/meetapp/static/js/2.6c62386b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbf04646e0819ab6a5c",
+    "url": "/meetapp/static/js/3.78155c6f.chunk.js"
+  },
+  {
+    "revision": "20ca87567928d131a7a3",
+    "url": "/meetapp/static/js/main.a0e68f20.chunk.js"
+  },
+  {
+    "revision": "ca1ca9a5d1e84ec06980",
+    "url": "/meetapp/static/js/runtime-main.3bd90e7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetapp/precache-manifest.71ad670ffdc59e8543cf0b220474cbee.js"
+  "/meetapp/precache-manifest.d9c57152d8a629dfcb736653b1c30328.js"
 );
 
 self.addEventListener('message', (event) => {
