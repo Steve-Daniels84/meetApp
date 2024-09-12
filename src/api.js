bf54619bb0a25750861e97d0c72407ebe0e8903f
Steve-Daniels84 <steve@bleachcyber.com>
2024-09-12 16:52:17 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,6 +7,7 @@ export const extractLocations = (events) => {
     return locations;
 };
 
+<<<<<<< HEAD
 export const checkToken = async (accessToken) => {
   const response = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -15,28 +16,60 @@ export const checkToken = async (accessToken) => {
   return result;
 };
 
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
+export const removeQuery = () => {
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+//fetches all events data
+export const getEvents = async () => {
+//   if (window.location.href.startsWith("http://localhost")) {
+//     return mockData;
+//   }
+
+  const token = await getToken();
+
+  if (token) {
+    removeQuery();
+    const url =
+      "https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/getCalendarEvents" +
+      "/" +
+      token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else return null;
+  }
+};
 
 export const getAccessToken = async () => {             
   const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-
-  }
-  return accessToken;
-}
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    'https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/token/{code}' + '/' + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-};
-
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://go95ldn5h7.execute-api.eu-west-2.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
   }
   return accessToken;
 };
@@ -60,9 +93,10 @@ const getToken = async (code) => {
     return error;
   }
 }; 
+=======
 
 //fetches all events data
 export const getEvents = async () => {
     return mockData
 }
-
+>>>>>>> parent of 548da68b (api inegration)
