8b0842700cd4e5c7dfc6247d1b1e4b312a38d19a
Steve-Daniels84 <steve@bleachcyber.com>
2024-09-23 14:23:11 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -45,6 +45,11 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
 
   const token = await getAccessToken();
 
@@ -54,6 +59,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
   }
